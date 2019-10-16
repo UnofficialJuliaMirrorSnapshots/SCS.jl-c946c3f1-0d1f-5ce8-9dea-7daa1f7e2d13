14f53e387400bf9f5b8061f95096eba4748b03ee
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-16 15:51:14 +0000

    Snapshot of branch master taken on 2019-10-16T11:51:13.005-04:00 from "https://github.com/UnofficialJuliaMirror/SCS.jl-c946c3f1-0d1f-5ce8-9dea-7daa1f7e2d13"

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -7,12 +7,12 @@ const MOIU = MOI.Utilities
 const MOIB = MOI.Bridges
 
 # UniversalFallback is needed for starting values
-const cache = MOIU.UniversalFallback(MOIU.Model{Float64}())
+const CACHE = MOIU.UniversalFallback(MOIU.Model{Float64}())
 
 import SCS
 
 for T in [SCS.Direct, SCS.Indirect]
-    optimizer = SCS.Optimizer(linear_solver=T, eps=1e-8)
+    optimizer = SCS.Optimizer(linear_solver=T, eps=1e-6)
     MOI.set(optimizer, MOI.Silent(), true)
 
     @testset "SolverName" begin
@@ -24,19 +24,19 @@ for T in [SCS.Direct, SCS.Indirect]
         @test !MOIU.supports_allocate_load(optimizer, true)
     end
 
-    MOI.empty!(cache)
-    cached = MOIU.CachingOptimizer(cache, optimizer)
+    MOI.empty!(CACHE)
+    cached = MOIU.CachingOptimizer(CACHE, optimizer)
     bridged = MOIB.full_bridge_optimizer(cached, Float64)
     config = MOIT.TestConfig(atol=1e-5)
 
     @testset "Unit" begin
         MOIT.unittest(bridged, config, [
+            # FIXME `NumberOfThreads` not supported.
+            "number_threads",
             # `TimeLimitSec` not supported.
             "time_limit_sec",
             # ArgumentError: The number of constraints in SCSModel must be greater than 0
             "solve_unbounded_model",
-            # Quadratic functions are not supported
-            "solve_qp_edge_cases",
             # Integer and ZeroOne sets are not supported
             "solve_integer_edge_cases", "solve_objbound_edge_cases",
             "solve_zero_one_with_bounds_1",
@@ -46,6 +46,11 @@ for T in [SCS.Direct, SCS.Indirect]
 
     @testset "Continuous linear problems with $T" begin
         MOIT.contlineartest(bridged, config)
+    end
+
+    @testset "ADMMIterations attribute with $T" begin
+        MOIT.linear1test(bridged, config)
+        @test MOI.get(bridged, SCS.ADMMIterations()) > 0
     end
 
     @testset "Continuous quadratic problems with $T" begin
